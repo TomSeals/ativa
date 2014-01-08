4e553994e8044317aa46d2a65880c4335d5c3b1e
erlaroche <E.R.Laroche@gmail.com>
2014-01-08 10:21:01 -0600

    Fix error routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Ativa::Application.routes.draw do
   devise_for :users
   resources :posts
   root 'projects#index'
-  resources :project 
+  resources :project do
     resources :subscriptions, only: [:create, :destroy]
   end
+  
 
   get 'users/:id' => 'users#show', as: 'user'
 
